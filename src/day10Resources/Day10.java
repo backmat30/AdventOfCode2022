6a1254ef19a09aa9470926152880bab25685be67
backmat30 <matthewbackman@outlook.com>
2022-12-10 20:02:19 -0600

    Day10 d10Part2 contains comments

--- src/day10Resources/Day10.java
+++ src/day10Resources/Day10.java
@@ -39,28 +39,35 @@ public class Day10 {
         System.out.println(totalSignal);
     }
     public static void d10Part2(List<String> commands){
+        //initial values
         int x = 1;
         int cycle = 0;
+        //2D list simulating the screen
         List<List<String>> screen = new ArrayList<>();
         screen.add(new ArrayList<>());
 
+        //runs each command
         for(String command: commands){
 
-
+            //splits command into the action and amount
             String[] commandWords = command.split(" ");
             if(commandWords[0].equals("addx")){
+                //SCREENHEIGHT constant to reduce clutter when grabbing the current row being made
                 final int SCREENHEIGHT = screen.size() - 1;
 
+                //checks if the value being added is within the "sprite" x
                 if(screen.get(SCREENHEIGHT).size() >= x - 1 && screen.get(SCREENHEIGHT).size() <= x + 1){
                     screen.get(SCREENHEIGHT).add("█");
                 } else{
                     screen.get(SCREENHEIGHT).add(" ");
                 }
+                //cycles then checks if it is the end of a row
                 cycle++;
                 if(cycle % 40 == 0){
                     screen.add(new ArrayList<>());
                 }
             }
+            //see lines 55-63
             final int SCREENHEIGHT = screen.size() - 1;
             if(screen.get(SCREENHEIGHT).size() >= x - 1 && screen.get(SCREENHEIGHT).size() <= x + 1){
                 screen.get(SCREENHEIGHT).add("█");
@@ -68,11 +75,12 @@ public class Day10 {
                 screen.get(SCREENHEIGHT).add(" ");
             }
 
-
+            //moves the position of the sprite if the command given is addx
             if(commandWords[0].equals("addx")){
 
                 x += Integer.parseInt(commandWords[1]);
             }
+            //see lines 64-68
             cycle++;
             if(cycle % 40 == 0){
                 screen.add(new ArrayList<>());
@@ -82,6 +90,7 @@ public class Day10 {
 
 
         }
+        //output
         for(List<String> row: screen){
             for(String pix: row){
                 System.out.print(pix);
